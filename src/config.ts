fb388690756625c127db12b4b488ef6bade13bcb
bittradercore <matthewcroughan@pm.me>
2018-12-06 11:51:25 +0000

    Update config.ts to TRD

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 let global : any = typeof window !== 'undefined' ? window : self;
 global.config = {
 	apiUrl:typeof window !== 'undefined' && window.location ? window.location.href.substr(0,window.location.href.lastIndexOf('/')+1)+'api/' : 'https://www.masariwallet.com/api/',
-	mainnetExplorerUrl: "https://msrchain.net/",
+	mainnetExplorerUrl: "https://explorer.bittrader.io,
 	mainnetExplorerUrlHash: "https://msrchain.net/tx/{ID}",
 	mainnetExplorerUrlBlock: "https://msrchain.net/block/{ID}",
 	testnetExplorerUrl: "http://testnet.msrchain.net/",
@@ -11,11 +11,11 @@ global.config = {
 	coinUnitPlaces: 12,
 	txMinConfirms: 10,         // corresponds to CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE in Monero
 	txCoinbaseMinConfirms: 60, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in Monero
-	addressPrefix: 28,
-	integratedAddressPrefix: 29,
+	addressPrefix: 38,
+	integratedAddressPrefix: 39,
 	addressPrefixTestnet: 33,
 	integratedAddressPrefixTestnet: 34,
-	subAddressPrefix: 52,
+	subAddressPrefix: 74,
 	subAddressPrefixTestnet: 73,
 	feePerKB: new JSBigInt('400000000'),//20^10 - for testnet its not used, as fee is dynamic.
 	dustThreshold: new JSBigInt('1000000000'),//10^10 used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
@@ -24,10 +24,10 @@ global.config = {
 	idleTimeout: 30,
 	idleWarningDuration: 20,
 
-	coinSymbol: 'MSR',
-	openAliasPrefix: "msr",
-	coinName: 'Masari',
-	coinUriPrefix: 'masari:',
+	coinSymbol: 'TRD',
+	openAliasPrefix: "trd",
+	coinName: 'Bittrader',
+	coinUriPrefix: 'bittrader:',
 	avgBlockTime: 120,
 	maxBlockNumber: 500000000,
 };
