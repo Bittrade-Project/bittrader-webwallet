b261e0380b68f252f951ae8b5886d2868053d4ae
bittradercore <matthewcroughan@pm.me>
2018-12-06 19:17:43 +0000

    Fix prefixes

--- src/config.ts
+++ src/config.ts
@@ -11,11 +11,11 @@ global.config = {
 	coinUnitPlaces: 12,
 	txMinConfirms: 10,         // corresponds to CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE in Monero
 	txCoinbaseMinConfirms: 60, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in Monero
-	addressPrefix: 38,
-	integratedAddressPrefix: 39,
+	addressPrefix: 28,
+	integratedAddressPrefix: 29,
 	addressPrefixTestnet: 33,
 	integratedAddressPrefixTestnet: 34,
-	subAddressPrefix: 74,
+	subAddressPrefix: 52,
 	subAddressPrefixTestnet: 73,
 	feePerKB: new JSBigInt('400000000'),//20^10 - for testnet its not used, as fee is dynamic.
 	dustThreshold: new JSBigInt('1000000000'),//10^10 used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
