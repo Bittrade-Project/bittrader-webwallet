88330f1dce3a2c36c98e21fc7617ff70bf5d8c52
bittradercore <matthewcroughan@pm.me>
2018-12-06 13:36:56 +0000

    change port config

--- src_api/config.php
+++ src_api/config.php
@@ -3,5 +3,5 @@
 $testnet = false;
 $cacheLocation = __DIR__.'/'.($testnet ? 'cache-testnet' : 'cache');
 $daemonAddress = 'localhost';
-$rpcPort = $testnet ? 48081 : 38081;
+$rpcPort = $testnet ? 48681 : 38681;
 $coinSymbol = 'trd';
